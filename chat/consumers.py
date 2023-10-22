137b0e442e35aaa63b230cbc7e4baad6482cc432
bigtorresg <bigtorresg@gmail.com>
2023-10-22 20:51:15 +0300

    message save and user add/remove added

--- chat/consumers.py
+++ chat/consumers.py
@@ -13,17 +13,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
         self.user = self.scope["user"]
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
-
+        await self.add_user(self.room_name, self.user)
         await self.accept()
 
     async def disconnect(self, close_code):
+        await self.remove_user(self.room_name, self.user)
         await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
 
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        user = self.user
+        room = self.room_name
         email = self.user.email
 
+        await self.save_message(room, user, message)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -38,3 +42,22 @@ class ChatConsumer(AsyncWebsocketConsumer):
         email = event["email"]
         message_html = f"<div hx-swap-oob='beforeend:#messages'><p><b>{email}</b>: {message}</p></div>"
         await self.send(text_data=json.dumps({"message": message_html, "email": email}))
+
+    @sync_to_async
+    def save_message(self, room, user, message):
+        room = ChatRoom.objects.get(title=room)
+        Message.objects.create(chat_room=room, sender=user, text=message)
+
+    @sync_to_async
+    def add_user(self, room, user):
+        room = ChatRoom.objects.get(title=room)
+        if user not in room.users.all():
+            room.users.add(user)
+            room.save()
+
+    @sync_to_async
+    def remove_user(self, room, user):
+        room = ChatRoom.objects.get(title=room)
+        if user in room.users.all():
+            room.users.remove(user)
+            room.save()
--- chat/routing.py
+++ chat/routing.py
@@ -1,4 +1,4 @@
 from django.urls import path
 from .consumers import ChatConsumer
 
-websocket_urlpatterns = [path("chat/<str:room_title>/", ChatConsumer.as_asgi())]
+websocket_urlpatterns = [path("chats/<str:room_title>/", ChatConsumer.as_asgi())]
